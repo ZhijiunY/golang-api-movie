3c16a0c0294709307480a558d73762532d823e48
ZhijiunY <yzj90596@gmail.com>
2023-02-10 23:56:02 +0800

    inint
    creat some simple struct, creat .apiConfig file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Zhijiun-api-movie
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+type apiConfigdata struct {
+	OpenMovieApiKey string `json:"OpenMovieApiKey"`
+}
+
+type movieData struct {
+	Name string
+	Year int
+}
